d1fd357ad2ff9f5ab1f7c098b5abdc6c3b12cf04
anaxfotopoulos <anaxsaroum@hotmail.com>
2015-11-13 23:58:34 +0200

    Created by Coding Ground Client

---
+++ New Project/mesos_oros.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+main()
+{
+    int a,b;
+    float c;
+    printf("dose 2 arithmous:");
+    scanf("%d %d",&a,&b);
+    c=(a+b)/2.0;
+    printf("o mesos oros toy %d kai toy %d ainai %.3f\n",a,b,c);
+	return 0;
+}
